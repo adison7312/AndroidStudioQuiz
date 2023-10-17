789be6cc028aa2f6c859de9712325a03052b3fc8
Adrian <adrian@test.pl>
2023-10-17 13:06:18 +0200

    added hints application lifecycle testing

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.quiz;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
     private int currentIndex = 0;
     private int score = 0;
+    private static final String KEY_CURRENT_INDEX = "current index";
+    public static final String KEY_EXTRA_ANSWER = "com.example.quiz.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private static boolean answerWasShown;
 
     private Question[] questions = new Question[] {
             new Question(R.string.question1, true),
@@ -32,20 +40,44 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(null, "Wywołana została metoda onSaveInstanceState()");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+        if(resultCode != RESULT_OK) {return;}
+        if(requestCode == REQUEST_CODE_PROMPT) {
+            if(data == null) {return;}
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(null, "Metoda wywołana w cyklu życia onCreate()");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        promptButton = findViewById(R.id.prompt_btn);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswerCorrectness(true);
                 currentIndex = (currentIndex + 1) % questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
@@ -55,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 checkAnswerCorrectness(false);
                 currentIndex = (currentIndex + 1) % questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
@@ -64,22 +97,70 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 checkResult();
                 currentIndex = (currentIndex + 1) % questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
 
+        promptButton.setOnClickListener( (v -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        }));
+
         setNextQuestion();
     }
+
+    //////////////////////////////////////////////
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(null, "Metoda wywołana w cyklu życia onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(null, "Metoda wywołana w cyklu życia onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(null, "Metoda wywołana w cyklu życia onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(null, "Metoda wywołana w cyklu życia onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(null, "Metoda wywołana w cyklu życia onDestroy()");
+    }
+
+    //////////////////////////////////////////////
+    //CYKL ZYCIA
+
 
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
 
-        if(userAnswer == correctAnswer) {
-            resultMessageId = R.string.correct_answer;
-            score++;
+        if(answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
         } else {
-            resultMessageId = R.string.incorrect_answer;
+            if(userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+                score++;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
 
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
